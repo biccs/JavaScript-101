e7857684da904b6fa345017e2fcb44ac12fa7089
biccs <biccsdev@gmail.com>
2022-03-29 02:37:41 -0700

    Working on the basics

---
+++ basic/arrays.js
@@ -0,0 +1,21 @@
+//(1)@biccsdev
+//JavaScript Arrays
+//Data Structures based on the Object element
+//it can contain multiple objects of different types.
+
+//Array Structure
+// "let" - reserved word stating it's scope
+// "array" - name of the variable
+// "[]" - square brackets used to represent the array, it
+// may or may not have value, if not value = undefined.
+let array = [1, "Value 2", 3.0];
+
+//Print whole array
+console.log(array);
+
+//Get element length
+console.log(array.length);
+
+//Access array elements
+//By Index [#] - position of element within array
+console.log(array[0])
--- basic/functions.js
+++ basic/functions.js
@@ -1,10 +1,16 @@
-//(1)@biccsdev
+//(0)@biccsdev
 //JavaScript Variables
 //Useful for reutilizing and structuring code, to give functionality
 // to classes, etc.
 //There are 2 types:
 
-//Declarative  Functions Structure
+//** Testing function declaration using Hoisting
+
+//We just call this function as it is, because
+// it already contains a method to handle the function value
+myFunct("Mexico");
+
+//Function Declaration Structure
 // "function" - reserved word
 // "myFunc()" - represents any given name that represents the function,
 // inside parenthesis should be the parameters if any
@@ -13,7 +19,7 @@ function myFunct(country) {
   console.log("Hello World from " + country);
 }
 
-//Functions as Expressions
+//Function Expression
 // "var" - reserved word for declaring variables with no scope
 // "myFunc" - variable name
 // "function" - reserved word for declaring a function it may or may not
@@ -23,13 +29,38 @@ var myFunc = function () {
   return "Hello World as a Expression";
 };
 
-//Testing
-
-//We just call this function as it is, because
-// it already contains a method to handle the function value
-myFunct("Mexico");
+//** MAIN DIFFERENCES **
+//We can call Function Declarations before declared in code as example above,
+// thanks to a concept called Hoisting.
+//With Function Expression we have to declare the function first and then call it.
 
 //Notice we call each function in a different way as
 // expression function returns a string, it need a method
 // to do something with its return value.
 console.log(myFunc());
+
+
+//** Switch function
+// used managing different possible outcomes
+// without the need of many if else statements.
+let value = 2;
+
+switch (value) {
+  case 1:
+    console.log(value);
+    break;
+  case 2:
+    console.log(value);
+  case 3:
+    console.log(value);
+    break;
+  case 4:
+    console.log(value);
+    break;
+  case 5:
+    console.log(value);
+  default:
+    console.log("error");
+}
+
+//** RUN on terminal with 'node variables.js' **
